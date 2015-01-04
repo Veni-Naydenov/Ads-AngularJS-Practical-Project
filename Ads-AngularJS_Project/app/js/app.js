bb61e84cb64552d8ba6be22354a12db446932315
Venelin <ipconfigv@gmail.com>
2015-01-04 21:46:41 +0200

    progress

--- Ads-AngularJS_Project/app/js/app.js
+++ Ads-AngularJS_Project/app/js/app.js
@@ -1,1 +1,21 @@
+'use strict';
 
+var adsApp = angular
+    .module('adsApp', ['ngResource', 'ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/home', {
+                templateUrl: 'views/templates/homeView.html',
+                controller: 'HomeController'
+            })
+            .when('/login', {
+               // templateUrl: 'views/templates/login.html',
+                controller: ''
+            })
+            .when('/register', {
+               // templateUrl: 'views/templates/register.html',
+                controller: ''
+            })
+
+            .otherwise({redirectTo: '/home'});
+    })
