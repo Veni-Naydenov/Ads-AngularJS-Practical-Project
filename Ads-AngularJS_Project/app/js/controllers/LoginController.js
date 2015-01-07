78908e5c7cd2cfb677dbee1196d57c22d8db7833
Venelin <ipconfigv@gmail.com>
2015-01-07 18:02:20 +0200

    login controller

--- Ads-AngularJS_Project/app/js/controllers/LoginController.js
+++ Ads-AngularJS_Project/app/js/controllers/LoginController.js
@@ -1,9 +1,25 @@
 'use strict';
 
 adsApp.controller('LoginController',
-    ['$scope', '$location', 'authentication', 'townsData', 'notifier',
+    ['$scope', '$location', 'authentication', 'notifier',
         function LoginController($scope, $location, authentication, notifier) {
             $scope.$emit('onMenuTitleChange', 'Login');
 
+            $scope.login = function (user, loginForm) {
+                if (loginForm.$valid) {
+                    authentication.login(user).then(function (success) {
+                        if (success) {
+                            notifier.success('Successful login!');
+                            $location.path('/');
+                        }
+                        else {
+                            notifier.error('Invalid login! Try again.');
+                        }
+                    });
+                }
+                else {
+                    notifier.error('Username and password are required!')
+                }
+            }
 
         }]);
