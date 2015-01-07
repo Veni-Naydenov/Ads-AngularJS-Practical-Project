bcb275e536e5f0aa112b86f369fa1adf70390aee
Venelin <ipconfigv@gmail.com>
2015-01-07 17:38:04 +0200

    login service

--- Ads-AngularJS_Project/app/js/services/authentication.js
+++ Ads-AngularJS_Project/app/js/services/authentication.js
@@ -1,30 +1,44 @@
 'use strict';
 
 adsApp.factory('authentication',
-    ['$http', '$q', 'baseServiceUrl','userIdentity','authorization',
-        function($http, $q, baseServiceUrl,userIdentity,authorization) {
-    var usersApi = baseServiceUrl + 'user/'
+    ['$http', '$q', 'baseServiceUrl', 'userIdentity', 'authorization',
+        function ($http, $q, baseServiceUrl, userIdentity, authorization) {
+            var usersApi = baseServiceUrl + 'user/'
 
-    return {
-        register: function(user) {
-            var deferred = $q.defer();
+            return {
+                register: function (user) {
+                    var deferred = $q.defer();
 
-            $http.post(usersApi + '/register', user)
-                .success(function() {
-                    deferred.resolve();
-                }, function(response) {
-                    deferred.reject(response);
-                })
+                    $http.post(usersApi + '/register', user)
+                        .success(function () {
+                            deferred.resolve();
+                        }, function (response) {
+                            deferred.reject(response);
+                        })
 
-            return deferred.promise;
-        },
-        login: function(user){
+                    return deferred.promise;
+                },
+                login: function (user) {
+                    var deferred = $q.defer();
 
-        },
-        logout: function() {
+                    $http.post(usersApi + '/login',user)
+                        .success(function (response) {
+                            if (response['access_token']) {
+                                userIdentity.setCurrentUser(response);
+                                authorization.setAuthorizationHeader(response['access_token']);
+                                deferred.resolve(true);
+                            }
+                            else {
+                                deferred.resolve(false);
+                            }
+                        });
 
-        }
+                    return deferred.promise;
+                },
+                logout: function () {
+
+                }
 
 
-    }
-}]);
+            }
+        }]);
