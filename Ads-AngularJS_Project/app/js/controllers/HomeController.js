8ee3a7317e137bc5a36a3cb9fdc0101df7d0bc34
Venelin <ipconfigv@gmail.com>
2015-01-08 02:37:26 +0200

    add loading gif

--- Ads-AngularJS_Project/app/js/controllers/HomeController.js
+++ Ads-AngularJS_Project/app/js/controllers/HomeController.js
@@ -3,11 +3,13 @@
 adsApp.controller('HomeController',
     function HomeController($scope, adsData, $log,notifier) {
         $scope.$emit('onMenuTitleChange', 'Home');
+        $scope.loaded=false;
 
         adsData.getAll()
             .$promise
             .then(function (data) {
                 $scope.data = data;
+                $scope.loaded=true;
             }, function (error) {
                 $log.error(error);
             })
